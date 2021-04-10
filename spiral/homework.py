34e09a4bb00b17abe9ae90cd1042a5ca6a52330e
negashramzi <70670861+negashramzi@users.noreply.github.com>
2021-04-09 21:53:45 -0400

    commit

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,15 @@
-def spiralize(number):
-    return_value = 1
+def sum_diagonals_of_spiral(size):
+    n = 1
+    step = 2
+    total = 0
+    since_last = 0
+    while n <= size**2:
+        total += n
+        n += step
+        since_last += 1
+        if since_last == 4:
+            step += 2
+            since_last = 0
+    return total
 
-    return return_value
+print(sum_diagonals_of_spiral(1001))
